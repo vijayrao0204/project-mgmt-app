7c5099a753440b88427e913218644885044eac86
vijay <vijay@bonhive.com>
2021-05-09 09:50:13 +0530

    my first commit

---
+++ src/main/java/com/bonhive/projectmgmtapp/ProjectMgmtAppApplication.java
@@ -0,0 +1,13 @@
+package com.bonhive.projectmgmtapp;
+
+    import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ProjectMgmtAppApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(ProjectMgmtAppApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/bonhive/projectmgmtapp/ProjectMgmtAppApplicationTests.java
@@ -0,0 +1,13 @@
+package com.bonhive.projectmgmtapp;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ProjectMgmtAppApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
